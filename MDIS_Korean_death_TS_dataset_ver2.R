c30da2f0c5d5b32cb3620570617019890ecb337e
Jongmin Oh <kard141@naver.com>
2022-05-06 18:53:50 +0900

    Update MDIS_Korean_death_TS_dataset_ver2.R

--- MDIS_Korean_death_TS_dataset_ver2.R
+++ MDIS_Korean_death_TS_dataset_ver2.R
@@ -45,10 +45,105 @@ tt29<-daily_count2(ts29);names(tt29)=paste0("D29_",names(tt29))
 tt30<-daily_count2(ts30);names(tt30)=paste0("D30_",names(tt30))
 tt31<-daily_count2(ts31);names(tt31)=paste0("D31_",names(tt31))
 
-names(ts01)
-version2<-cbind(ts01 %>% select(key:dow),tt01,tt02,tt03,tt04,tt05,tt06,tt07,tt08,tt09,tt10,
-                tt11,tt12,tt13,tt14,tt15,tt16,tt17,tt18,tt19,tt20,
-                tt21,tt22,tt23,tt24,tt25,tt26,tt27,tt28,tt29,tt30,ts31 %>% select(mintemp:meanpress2))
+names(tt01)=gsub("D01","intestinal_infec",names(tt01))
+names(tt02)=gsub("D02","circ"            ,names(tt02))
+names(tt03)=gsub("D03","angina"          ,names(tt03))
+names(tt04)=gsub("D04","MI"              ,names(tt04))
+names(tt05)=gsub("D05","ischHD"          ,names(tt05))
+names(tt06)=gsub("D06","cerebvas"        ,names(tt06))
+names(tt07)=gsub("D07","resp"            ,names(tt07))
+names(tt08)=gsub("D08","acuteup"         ,names(tt08))
+names(tt09)=gsub("D09","pneum"           ,names(tt09))
+names(tt10)=gsub("D10","asthma"          ,names(tt10))
+names(tt11)=gsub("D11","voldep"          ,names(tt11))
+names(tt12)=gsub("D12","heat"            ,names(tt12))
+names(tt13)=gsub("D13","heatrelated"     ,names(tt13))
+names(tt14)=gsub("D14","frost"           ,names(tt14))
+names(tt15)=gsub("D15","hypothermia"     ,names(tt15))
+names(tt16)=gsub("D16","otherredutemp"   ,names(tt16))
+names(tt17)=gsub("D17","frostrelated"    ,names(tt17))
+names(tt18)=gsub("D18","aki"             ,names(tt18))
+names(tt19)=gsub("D19","suicide"         ,names(tt19))
+names(tt20)=gsub("D20","mental"          ,names(tt20))
+names(tt21)=gsub("D21","all"             ,names(tt21))
+names(tt22)=gsub("D22","nonacc"          ,names(tt22))
+names(tt23)=gsub("D23","hemoStroke"      ,names(tt23))
+names(tt24)=gsub("D24","ischStroke"      ,names(tt24))
+names(tt25)=gsub("D25","otherStroke"     ,names(tt25))
+names(tt26)=gsub("D26","TIA"             ,names(tt26))
+names(tt27)=gsub("D27","acutelow"        ,names(tt27))
+names(tt28)=gsub("D28","copd"            ,names(tt28))
+names(tt29)=gsub("D29","dm"              ,names(tt29))
+names(tt30)=gsub("D30","kidney"          ,names(tt30))
+names(tt31)=gsub("D31","accd"            ,names(tt31))
 
-# setwd("D:\\SNU\\기후보건영향평가\version2")
-# write.csv(version2,file="version2.csv",row.names=F,na="")
+names(version2)
+
+version2<-cbind(ts01 %>% select(key,year:dow),tt01,tt02,tt03,tt04,tt05,tt06,tt07,tt08,tt09,tt10,
+                tt11,tt12,tt13,tt14,tt15,tt16,tt17,tt18,tt19,tt20,
+                tt21,tt22,tt23,tt24,tt25,tt26,tt27,tt28,tt29,tt30,tt31,ts31 %>% select(mintemp:pm25_model))
+
+sido_df<-data.frame(sido=c(11,21:25,26,29,31:39),
+                    area=c("서울","부산","대구","인천","광주","대전","울산","세종","경기","강원","충북","충남","전북",
+                               "전남","경북","경남","제주"),
+                    EN_SIDO=c("seoul","busan","daegu","incheon","gwangju","daejeon","ulsan","sejong","ggyeonggi","gangwon","chungbuk","chungnam",
+                              "jeonbuk","jeonnam","gyeongbuk","gyeongnam","jeju"))
+
+version2$area=substr(version2$key,1,2)
+version2<-version2 %>% left_join(sido_df,by="area")
+head(version2)
+version2$ddate=ymd(substr(version2$key,4,13))
+
+#serial number
+version2$ddd=as.numeric(as.factor(version2$ddate))
+
+version2<-version2 %>% select(ddate,year,month,day,ddd,sido,dow,area,EN_SIDO,
+                              all_tot             ,all_age0005             ,all_age0014             ,all_age1564             ,all_age65,
+                              nonacc_tot          ,nonacc_age0005          ,nonacc_age0014          ,nonacc_age1564          ,nonacc_age65,
+                              circ_tot            ,circ_age0005            ,circ_age0014            ,circ_age1564            ,circ_age65,
+                              resp_tot            ,resp_age0005            ,resp_age0014            ,resp_age1564            ,resp_age65,
+                              ischHD_tot          ,ischHD_age0005          ,ischHD_age0014          ,ischHD_age1564          ,ischHD_age65,
+                              cerebvas_tot        ,cerebvas_age0005        ,cerebvas_age0014        ,cerebvas_age1564        ,cerebvas_age65,
+                              angina_tot          ,angina_age0005          ,angina_age0014          ,angina_age1564          ,angina_age65,
+                              MI_tot              ,MI_age0005              ,MI_age0014              ,MI_age1564              ,MI_age65,
+                              hemoStroke_tot      ,hemoStroke_age0005      ,hemoStroke_age0014      ,hemoStroke_age1564      ,hemoStroke_age65,
+                              ischStroke_tot      ,ischStroke_age0005      ,ischStroke_age0014      ,ischStroke_age1564      ,ischStroke_age65,
+                              otherStroke_tot     ,otherStroke_age0005     ,otherStroke_age0014     ,otherStroke_age1564     ,otherStroke_age65,
+                              TIA_tot             ,TIA_age0005             ,TIA_age0014             ,TIA_age1564             ,TIA_age65,
+                              pneum_tot           ,pneum_age0005           ,pneum_age0014           ,pneum_age1564           ,pneum_age65,
+                              acutelow_tot        ,acutelow_age0005        ,acutelow_age0014        ,acutelow_age1564        ,acutelow_age65,
+                              asthma_tot          ,asthma_age0005          ,asthma_age0014          ,asthma_age1564          ,asthma_age65,
+                              copd_tot            ,copd_age0005            ,copd_age0014            ,copd_age1564            ,copd_age65,
+                              acuteup_tot         ,acuteup_age0005         ,acuteup_age0014         ,acuteup_age1564         ,acuteup_age65,
+                              heat_tot            ,heat_age0005            ,heat_age0014            ,heat_age1564            ,heat_age65,
+                              voldep_tot          ,voldep_age0005          ,voldep_age0014          ,voldep_age1564          ,voldep_age65,
+                              heatrelated_tot     ,heatrelated_age0005     ,heatrelated_age0014     ,heatrelated_age1564     ,heatrelated_age65,
+                              frost_tot           ,frost_age0005           ,frost_age0014           ,frost_age1564           ,frost_age65,
+                              hypothermia_tot     ,hypothermia_age0005     ,hypothermia_age0014     ,hypothermia_age1564     ,hypothermia_age65,
+                              otherredutemp_tot   ,otherredutemp_age0005   ,otherredutemp_age0014   ,otherredutemp_age1564   ,otherredutemp_age65,
+                              frostrelated_tot    ,frostrelated_age0005    ,frostrelated_age0014    ,frostrelated_age1564    ,frostrelated_age65,
+                              aki_tot             ,aki_age0005             ,aki_age0014             ,aki_age1564             ,aki_age65,
+                              dm_tot              ,dm_age0005              ,dm_age0014              ,dm_age1564              ,dm_age65,
+                              kidney_tot          ,kidney_age0005          ,kidney_age0014          ,kidney_age1564          ,kidney_age65,
+                              accd_tot            ,accd_age0005            ,accd_age0014            ,accd_age1564            ,accd_age65,
+                              suicide_tot         ,suicide_age0005         ,suicide_age0014         ,suicide_age1564         ,suicide_age65,
+                              mental_tot          ,mental_age0005          ,mental_age0014          ,mental_age1564          ,mental_age65,
+                              intestinal_infec_tot,intestinal_infec_age0005,intestinal_infec_age0014,intestinal_infec_age1564,intestinal_infec_age65,
+                    
+                    all_m    ,nonacc_m,circ_m,resp_m,ischHD_m,
+                    cerebvas_m,angina_m,MI_m,hemoStroke_m,ischStroke_m,otherStroke_m,
+                    TIA_m,pneum_m,acutelow_m,asthma_m,copd_m,
+                    acuteup_m,heat_m,voldep_m,heatrelated_m,frost_m,hypothermia_m,
+                    otherredutemp_m,frostrelated_m,aki_m,dm_m,kidney_m,accd_m,suicide_m,mental_m,intestinal_infec_m,
+                    all_f    ,nonacc_f,circ_f,resp_f,ischHD_f,
+                    cerebvas_f,angina_f,MI_f,hemoStroke_f,ischStroke_f,otherStroke_f,
+                    TIA_f,pneum_f,acutelow_f,asthma_f,copd_f,
+                    acuteup_f,heat_f,voldep_f,heatrelated_f,frost_f,hypothermia_f,
+                    otherredutemp_f,frostrelated_f,aki_f,dm_f,kidney_f,accd_f,suicide_f,mental_f,intestinal_infec_f,
+                    
+                    mintemp:pm25_model)
+
+names(version2)[238:242]=c("pm25","pm10","so2","no2","co")
+
+setwd("D:\\SNU\\연구\\질병관리본부\\기후보건영향평가_평가체계구축및시범사업\\2022\\자료\\통계청\\version2")
+write.csv(version2,file="version2.csv",row.names=F,na="")
